4df65231ee89ba70f1ea4ebe2b46e0c2b74fd92f
JacksonVago <jackson@natividadesolucoes.com.br>
2024-10-10 17:35:37 -0300

    Origins

--- Startup.cs
+++ Startup.cs
@@ -61,7 +61,7 @@ namespace WebAppServer
                                       policy =>
                                       {
                                           policy.WithOrigins("http://localhost:3000",
-                                                             "http://3.94.130.80",
+                                                             "http://107.22.14.181",
                                                              "http://35.174.17.110");
                                           policy.AllowAnyHeader();
                                       });
