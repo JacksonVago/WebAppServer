c2414df432924dc308d212c1a5ebdd01bf3f44e3
JacksonVago <jackson@natividadesolucoes.com.br>
2025-05-26 16:34:11 -0300

    Incio de Dia

--- Services/ScheduleTaks.cs
+++ Services/ScheduleTaks.cs
@@ -1,23 +1,36 @@
 ﻿using Coravel.Invocable;
 using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
+using System;
 using System.Threading.Tasks;
+using WebAppServer.Models;
+using WebAppServer.Repositories;
 
 namespace WebAppServer.Services
 {
     public class ScheduleTaks : IInvocable
     {
         private readonly ILogger<ScheduleTaks> _logger;
+        private readonly LocalRepository _local;
 
-        public ScheduleTaks(ILogger<ScheduleTaks> logger)
+        public ScheduleTaks(ILogger<ScheduleTaks> logger, LocalRepository local)
         {
             _logger = logger;
+            _local = local;
         }
 
         public async Task Invoke()
         {
-            _logger.LogInformation("Hello");
-            throw new System.NotImplementedException();
+            //_logger.LogInformation("Hello");
+            //throw new System.NotImplementedException();
+            try
+            {
+                _local.IniciaDia();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
         }
     }
 }
